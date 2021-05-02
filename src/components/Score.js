5d38180ba6cfe88d9dad748d04906020462bb2fd
Swaup <swarups142@gmail.com>
2021-05-02 13:29:48 +0530

    add now scripts

--- src/components/Score.js
+++ src/components/Score.js
@@ -64,7 +64,7 @@ class Score extends Component {
     getMatchCart = () => {
         let score = this.getScore(this.state.detail.score);
         return (
-            <div className="scoring">
+   
             <Card className={this.props.classes.cardstyle} style={{ marginTop: 20, width: 1000,  height: 'auto' }}>
                 <Box className={this.props.classes.component}>
                     <Typography style={{fontFamily: 'Serif', fontSize: '17px'}}>{this.props.match["team-1"]} vs {this.props.match["team-2"]}</Typography>
@@ -96,7 +96,7 @@ class Score extends Component {
                     </Grid>
                 </CardActions>
             </Card>
-            </div>
+            
         );
     }
 
